68c482ad5a3fb9f23be9e982eb83c538d081438d
ks563 <karen.shea90@gmail.com>
2019-04-19 20:47:52 -0500

    creating card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Container>
+            <Card />
+            <Card />
+            <Card />
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+
+          </Container>
+
+        </Wrapper>
+        <Footer/>
+
       </div>
+
+      
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
     );
   }
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Card extends Component {
+    super(props)
+
+    return (
+        <div 
+            className="card"
+            style={{
+                backgroundImage: props.image ? `url$(${props.image})` : "none"
+            }}
+        >
+         {/* set onClick for the entire card */}
+        </div>
+    )
+}
+
+export default Card;
