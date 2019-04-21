4b0b6112e41ad78cae1e1ced7860e550e0b5ca38
ks563 <karen.shea90@gmail.com>
2019-04-20 19:32:51 -0500

    attempting to fix json issues, won't read data file

--- src/App.js
+++ src/App.js
@@ -7,135 +7,78 @@ import Col from "./components/Col";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
-import images from "./images.json"
+import data from './data.json';
 import './App.css';
 
 
 class App extends Component {
-  state={
+  state = {
     score: 0,
     highScore: 0
   }
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  }
+  //need code to track when an image has been clicked 
 
-  clickPicture = id => {
-    shuffleArray = (array) => {
-      let i = array.length - 1;
-      for (; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
-    }
+
+  clickPicture = event => {
+//changes score and highScore
+      //click triggers shuffle array
     
-
   }
 
+  shuffleArray = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
 
   render() {
     return (
       <div>
         {/* <Navbar /> */}
-          <Jumbotron/>
+        <Jumbotron />
         <Container>
-        <Wrapper>
-          <Row>
-              <Col>
-                 
-                  {this.state.images.map(image => {
-                    
-                    return (
-                      <Card
-                      id = { images.id }
-                        picture={images.image}
-                        />
-                    )
-                  
-                  }
-                  )}
-               
-
-              {/* <Card
-                id={images[0].id}
-                  image={images[0].image}
-                  
-              />
-               <Card
-                id={images[1].id}
-                image={images[1].image}  
-              />
-               <Card
-                id={images[2].id}
-                image={images[2].image}  
-              />
-               <Card
-                id={images[3].id}
-                image={images[3].image}  
-              />
-               <Card
-                id={images[4].id}
-                image={images[4].image}  
-              />
-               <Card
-                id={images[5].id}
-                image={images[5].image}  
-              />
-               <Card
-                id={images[6].id}
-                image={images[6].image}  
-              />
-               <Card
-                id={images[7].id}
-                image={images[7].image}
-              />
-               <Card
-                id={images[8].id}
-                image={images[8].image}  
-              />
-               <Card
-                id={images[9].id}
-                image={images[9].image}  
-              />
-               <Card
-                id={images[10].id}
-                image={images[10].image}   */}
-              />
-            </Col>
-          </Row>
+          <Wrapper>
+            {this.state.data.map(data => {
+              return (
+                <Card
+                  id={data.id}
+                  picture={data.image}
+                />
+              )
+            }
+            )}
           </Wrapper>
-          </Container>
-          
+        </Container>
 
-     
-        <Footer/>
+
+
+        <Footer />
 
       </div>
 
-      
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <p>
-    //         Edit <code>src/App.js</code> and save to reload.
-    //       </p>
-    //       <a
-    //         className="App-link"
-    //         href="https://reactjs.org"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Learn React
-    //       </a>
-    //     </header>
-    //   </div>
+
+      //   <div className="App">
+      //     <header className="App-header">
+      //       <img src={logo} className="App-logo" alt="logo" />
+      //       <p>
+      //         Edit <code>src/App.js</code> and save to reload.
+      //       </p>
+      //       <a
+      //         className="App-link"
+      //         href="https://reactjs.org"
+      //         target="_blank"
+      //         rel="noopener noreferrer"
+      //       >
+      //         Learn React
+      //       </a>
+      //     </header>
+      //   </div>
     );
   }
 }
